8d4724c709dd7f7c4828d1b78823bf008daf68d9
Saurav Gurhale <slgurhal@uwaterloo.ca>
2021-06-29 07:49:16 -0400

    find diffrence between the 2 files

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,33 +3,36 @@ const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
-let books = [{id: "1", name: "BOOK1", genre: "GENRE1"}, 
-            {id: "2", name: "BOOK2", genre: "GENRE2"}, 
-            {id: "1", name: "BOOK3", genre: "GENRE3"}];
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
 
-const BookType = new GraphQLObjectType({ 
+const BookType = new GraphQLObjectType({
     name: 'Book',
-    fields: () => ({
-        id: { type: GraphQLString }, 
+    fields: ( ) => ({
+        id: { type: GraphQLString },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString },
+        genre: { type: GraphQLString }
     })
- })
+});
 
- const RootQuery = new GraphQLObjectType({
-     name: 'RootQueryType',
-     fields: {
-         book: {
-             type: BookType,
-             arg: { id: { type: GraphQLString } },
-             resolve(parent, args)      {
-                 // code to get data from db
-                 return _.find(books, { id: args.id });
-             }
-         }
-     }
- });
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args){
+                // code to get data from db / other source
+                return _.find(books, { id: args.id });
+            }
+        }
+    }
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery    
+    query: RootQuery
 });
